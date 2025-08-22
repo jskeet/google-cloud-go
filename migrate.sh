71fa4d7500593c864f9e5af15789dfafa4cc8d89
Jon Skeet <jonskeet@google.com>
2025-08-22 07:20:04 +0000

    update migrate script

--- migrate.sh
+++ migrate.sh
@@ -5,4 +5,4 @@ set -e
 chmod -x $(find . -name '*.go')
 goimports -w .
 sed -i -E 's/Copyright [0-9]+/Copyright 2025/g' $(find . -name version.go)
-grep '//go:build ignore' -l $(find . -name '*.go') | xargs rm
+grep '//go:build ignore' -l $(find . -name '*_grpc.pb.go') | xargs rm
